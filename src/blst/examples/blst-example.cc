1a7f6c60c356c63b17f09c116c43d022531fbaf7
vlado <vlada.rocin@gmail.com>
2021-07-30 12:52:57 +0200

    fixup

--- src/blst/examples/blst-example.cc
+++ src/blst/examples/blst-example.cc
@@ -3,7 +3,7 @@
 #include "ns3/core-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/blst.h"
-#include "ns3/myresultapp.h"
+#include "ns3/bloom_filter.hpp"
 
 #include <stdio.h>
 #include <assert.h>
@@ -27,11 +27,12 @@ P1_Affine aggregate_signatures(vector<P1_Affine> signatures)
 
 vector<uint8_t> extractByteVector(bloom_filter filter)
 {
-    return vector<uint8_t> ((uint8_t*)filter.table(), (uint8_t*)filter.table() + filter.size());
+    return vector<uint8_t>((uint8_t*)filter.table(), (uint8_t*)filter.table() + filter.size());
 }
 
 int main(int argc, char* argv[])
 {
+    static const std::size_t UNIVERSAL_SEED = 10;
     float p = 0.02;
     int n = 200;
     // int m = 15;
@@ -42,7 +43,7 @@ int main(int argc, char* argv[])
     string str3 = "content3";
     string str4 = "content4";
 
-    Ptr<MyBloom_filter> myfilter = CreateObject<MyBloom_filter>(IBF_PEC, FPP, ns3::UNIVERSAL_SEED);
+    //Ptr<MyBloom_filter> myfilter = CreateObject<MyBloom_filter>(IBF_PEC, FPP, ns3::UNIVERSAL_SEED);
 
     bloom_filter filter1 = bloom_filter(n, p, UNIVERSAL_SEED);
     bloom_filter filter2 = bloom_filter(n, p, UNIVERSAL_SEED);
@@ -51,18 +52,31 @@ int main(int argc, char* argv[])
     filter1.insert(str1);
     filter2.insert(str2);
     filter3.insert(str3);
+
+    if (filter1.contains(str1)) {
+        std::cout << "filter1 contains " << str1 << std::endl;
+    }
+    else {
+        std::cout << "ERROR!!!! filter1 DOES NOT contain " << str1 << std::endl;
+    }
     srand(25);
     uint8_t seed[32] = { 0 };
 
-    vector<uint8_t> v1 ((uint8_t*)filter1.table(), (uint8_t*)filter1.table() + filter1.size());
+    vector<uint8_t> v1((uint8_t*)filter1.table(), (uint8_t*)filter1.table() + filter1.size());
     uint8_t msg1[v1.size()];
     copy(v1.begin(), v1.end(), msg1);
 
-    vector<uint8_t> v2 ((uint8_t*)filter2.table(), (uint8_t*)filter2.table() + filter2.size());
+    bloom_filter bf4 = filter1;
+
+    if (bf4.contains(str1)) {
+        std::cout << "bf4 contains " << str1 << std::endl;
+    }
+
+    vector<uint8_t> v2((uint8_t*)filter2.table(), (uint8_t*)filter2.table() + filter2.size());
     uint8_t msg2[v2.size()];
     copy(v2.begin(), v2.end(), msg2);
 
-    vector<uint8_t> v3 ((uint8_t*)filter3.table(), (uint8_t*)filter3.table() + filter3.size());
+    vector<uint8_t> v3((uint8_t*)filter3.table(), (uint8_t*)filter3.table() + filter3.size());
     uint8_t msg3[v3.size()];
     copy(v3.begin(), v3.end(), msg3);
 
